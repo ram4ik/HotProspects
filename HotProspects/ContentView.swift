023f879d48512aa14558a7d746fd1c5bab912bbf
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-03 09:42:24 +0200

    Controlling image interpolation in SwiftUI

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,30 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
-    
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-    
-    func fetchReadings() async {
-        let fetchTask = Task {
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        let result = await fetchTask.result
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Error \(error.localizedDescription)"
+        VStack {
+            Image(systemName: "bird")
+                .interpolation(.none)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.white)
+                .background(.black)
+                .mask {
+                    Circle()
+                }
+                .padding()
+            
+            Text("The Bird")
+                .font(.largeTitle)
+                .bold()
         }
     }
 }
